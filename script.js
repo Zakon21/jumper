1d37e2594a7f989dc490b9e3167a94715e35dfbc
Reckfy <reckfy777@gmail.com>
2024-06-11 23:28:57 +0300

    addon1

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict'
+const startButton = document.getElementById('menu')
+startButton.addEventListener('click', (x) => {
+    startButton.innerHTML = 'GO!';
+    //document.getElementsByClassName('stone')[0].style.left = x + 'px';
+});
